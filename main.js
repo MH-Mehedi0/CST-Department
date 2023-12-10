5f8fdccb3cdbdd499fdb57e07a2c7c52f770c316
MH-Mehedi0 <144585649+MH-Mehedi0@users.noreply.github.com>
2023-12-10 16:22:45 +0600

    Update JS file, Css file, index file

---
+++ main.js
@@ -0,0 +1,27 @@
+<!-- Toggle Menu-------->
+    
+        var navLinks = document.getElementById("navLinks");
+        
+        function showMenu(){
+            navLinks.style.right = "0";
+        }
+        
+        function hideMenu(){
+            navLinks.style.right = "-200px";
+        }
+        
+    
+//    Pagination--------
+
+//let link = document.getElementsByClassName("link");
+//
+//let currentValue = 1;
+//
+//function activeLink(){
+//    
+//    for(1 of link){
+//        1.classList.remove("active");
+//    }
+//}
+
+    
