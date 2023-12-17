5b2b09792021d7e2f7b9956e880462b5789b17ab
MH-Mehedi0 <144585649+MH-Mehedi0@users.noreply.github.com>
2023-12-17 18:55:32 +0600

    full website uploded

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<!-- Toggle Menu-------->
+//<!-- Toggle Menu-------->
     
         var navLinks = document.getElementById("navLinks");
         
@@ -9,19 +9,20 @@
         function hideMenu(){
             navLinks.style.right = "-200px";
         }
+   
+//Typed Function
+
+$(function(){
+    $(".typed").typed({
+        strings: ["মনোরম পরিবেশ। ", " আধুনিক কম্পিউটার লেব। ", "উন্নত মানের সফটওয়্যার লেব। ","উন্নত মানের নেটওয়ার্ক  লেব। ","উন্নত মানের হার্ডওয়্যার লেব। ", "স্কিল্ড শিক্ষক মন্ডলি। ", "সিসি কেমেরার আউতায় পর্যবেক্ষণ। "],
+        loop: true,
+        startDelay: 500,
+        typeSpeed: 80,
+        backDelay: 1000,
+        backSpeed:80,
+        showCursor: true
         
-    
-//    Pagination--------
-
-//let link = document.getElementsByClassName("link");
-//
-//let currentValue = 1;
-//
-//function activeLink(){
-//    
-//    for(1 of link){
-//        1.classList.remove("active");
-//    }
-//}
-
+        
+    })
+})
     
